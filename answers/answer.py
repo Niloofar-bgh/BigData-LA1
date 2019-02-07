7c610e177e65edaf1532a959d3f3f42d8b02fc3b
Niloofar-bgh <n_baghda@orwell.encs.concordia.ca>
2019-02-07 16:54:38 -0500

    uniq_park_count_rdd update 3

--- answers/answer.py
+++ answers/answer.py
@@ -337,6 +337,7 @@ def uniq_parks_counts_rdd(filename):
     from pyspark import SparkConf
     from pyspark.sql import SQLContext
 
+
     sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
     sql_context = SQLContext(sc)
     rdd = (sql_context.read.format('com.databricks.spark.csv').option("header", "true").load(filename)).rdd
